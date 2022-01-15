72aaee9e3186cdae312d7e182ea9c55e49947aa4
BrandonC98 <brandoncampbell98@hotmail.co.uk>
2022-01-15 21:40:57 +0000

    Adds command line args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
 use error_chain::error_chain;   
+use clap::Parser;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let args = Args::parse();
+
+    let res = reqwest::get(args.url)
+    .await?
+    .text()
+    .await?;
+    
+    println!("{}", res.as_str());
+    
+    //   Document::from(res.as_str())
+    //     .find(Name("a"))
+    //     .filter_map(|n| n.attr("href"))
+    //     .for_each(|x| println!("{}", x));
+    
+    Ok(())
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+
+    #[clap(short = 'u', long = "url")]
+    url: String
+
+}
 
 error_chain! {
       foreign_links {
@@ -6,21 +37,3 @@ error_chain! {
           IoError(std::io::Error);
       }
 }
-
-#[tokio::main]
-async fn main() -> Result<()> {
-  let res = reqwest::get("https://www.rust-lang.org")
-    .await?
-    .text()
-    .await?;
-
-    println!("{}", res.as_str());
-
-//   Document::from(res.as_str())
-//     .find(Name("a"))
-//     .filter_map(|n| n.attr("href"))
-//     .for_each(|x| println!("{}", x));
-
-  Ok(())
-}
-
