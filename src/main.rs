a2cd6759982115ca4fecdcf3eae2f22aaf5c8a63
BrandonC98 <brandoncampbell98@hotmail.co.uk>
2022-01-15 21:19:25 +0000

    Adds basic request functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;   
+
+error_chain! {
+      foreign_links {
+          ReqError(reqwest::Error);
+          IoError(std::io::Error);
+      }
 }
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let res = reqwest::get("https://www.rust-lang.org")
+    .await?
+    .text()
+    .await?;
+
+    println!("{}", res.as_str());
+
+//   Document::from(res.as_str())
+//     .find(Name("a"))
+//     .filter_map(|n| n.attr("href"))
+//     .for_each(|x| println!("{}", x));
+
+  Ok(())
+}
+
